7cfcbe900ee47e3b5868424f25492e32bf161442
Kennedy Damaceno <kennedy_dos@hotmail.com>
2023-02-27 18:44:59 -0300

    Exercicio de mostrar operações na tela

---
+++ Fundamentos/Secao-3-Introdução-a-JavaScript/dia-1-JavaScript-Primeiros-passos/exercicio1.js
@@ -0,0 +1,8 @@
+let a = 8;
+let b = 11;
+
+console.log('Soma: '+ (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Modulo: ' + (a % b));
