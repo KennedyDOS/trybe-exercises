db48b490147a71f24fa8feb759040087f645cf7a
Kennedy Damaceno <kennedy_dos@hotmail.com>
2023-03-02 18:17:02 -0300

    Exercicio 1 e 2 de objetos e for/in

---
+++ Fundamentos/Secao-3-Introdução-a-JavaScript/dia-4-JavaScript-Objetos/exercicio-for-in.js
@@ -0,0 +1,21 @@
+/// Exercicio 1!
+
+/*let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('BBem vinda, ' + info.personagem + '.' );  */
+
+/// Exercicio 2
+
+let info = {
+        personagem: 'Margarida',
+        origem: 'Pato Donald',
+        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+        recorrente: 'Sim'
+  };
+
+  info.recorrente = 'Não';
+  console.log(info.recorrente);
