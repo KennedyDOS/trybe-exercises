75c5dc66e88727e4f393286bfafa2f26edc25c4a
Kennedy Damaceno <kennedy_dos@hotmail.com>
2023-03-04 23:19:23 -0300

    Resto dos exercicios de primeiros passos do JavaScript

---
+++ Fundamentos/Secao-3-Introdução-a-JavaScript/dia-1-JavaScript-Primeiros-passos/um-impar-entre-nos.js
@@ -0,0 +1,10 @@
+let num1 = 8;
+let num2 = 8;
+let num3 = 8;
+let parOuImpar = false;
+
+if ((num1 % 3 !== 0 || num2 % 3 !== 0 || num3 % 3 !== 0)){
+    parOuImpar = true;
+};
+
+console.log(parOuImpar);
