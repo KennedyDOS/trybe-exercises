7e4adbd7e891ce16366bf97958d404d5f02a88bc
Kennedy Damaceno <kennedy_dos@hotmail.com>
2023-04-10 18:53:31 -0300

    Fixações e exercicios do ultimo dia do modulo

---
+++ Fundamentos/Secao-7-High-Order-Functions-do-JavaScript-ES6/7.5-JS-ES6-Array-destructuring-Default-Destructuring/fixacaoDestructuring1.js
@@ -0,0 +1,12 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]) // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [firstPrime, secondPrime, thirdPrime] = primeNumbers;
+
